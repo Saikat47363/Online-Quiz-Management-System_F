5f2d7c5f5b6a63bf848b031245caac86582049a7
LINNAS-JIM <shoumunl01@gmail.com>
2025-09-17 17:35:36 +0600

    Revert "Jim"
    This reverts commit dbf0d38c1e9997ea0250ac32c73d233c85d6f9c4.

--- utils/validation.ts
+++
@@ -1,9 +0,0 @@
-export const validateUser = (data: any) => {
-  const errors: any = {};
-  if (!/^[A-Za-z ]+$/.test(data.name)) errors.name = "Name must be letters only";
-  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) errors.email = "Invalid email";
-  if (!/^\d{11}$/.test(data.phone)) errors.phone = "Phone must be 11 digits";
-  if (!data.password || data.password.length < 6) errors.password = "Password min 6 chars";
-  if (!data.country) data.country = "Bangladesh";
-  return errors;
-};
